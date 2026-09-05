39c9f7d53c4ca3f873bc46ebb9d691d38fda849c
Mahdi <mahditaghdisi650@gmail.com>
2026-09-05 12:12:17 +0330

    fix 2

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4dr)gz!njie@7*1u&*)7^b_s5-a!7p+-poiu5ks!nvd&k-g5p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.145','192.168.42.100','10.232.230.32','0.0.0.0']
 
